9351de6c628ff420e449f984f7fb1a1d03832fb3
motephyr <motephyr@gmail.com>
2021-01-28 11:09:47 +0800

    Set menu, index, and demo page style

--- start/routes.js
+++ start/routes.js
@@ -19,4 +19,5 @@ require('./authRoutes.js');
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('page/index')
+Route.on('/demo').render('page/demo')
