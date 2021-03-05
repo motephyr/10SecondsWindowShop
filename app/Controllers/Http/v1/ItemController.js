8a8d9edcc6bffe8bcf73865d1dc27e50205fe4b8
motephyr <motephyr@gmail.com>
2021-03-06 05:01:27 +0800

    add ffmpeg

--- app/Controllers/Http/v1/ItemController.js
+++ app/Controllers/Http/v1/ItemController.js
@@ -2,16 +2,18 @@
 
 const Item = use('App/Models/Item')
 const Drive = use('Drive')
-const { validate } = use("Validator");
 const Record = use('App/Models/Record')
+const fs = require('fs')
+const ffmpeg = require('fluent-ffmpeg');
 
 class ItemController {
-  async index({ response, auth }) {
+  async index({request, response, auth }) {
     auth = auth.authenticator('jwt')
+    const { kind } = request.all()
     let items
-    if(auth.user){
+    if (auth.user && kind === 'isBuyerPage') {
       items = await auth.user.buyerItems().orderBy('id').fetch()
-    }else{
+    } else {
       items = await Item.query().where({ status: 'publish' }).orderBy('id').fetch()
     }
     return response.send({ items: items.rows });
@@ -47,53 +49,43 @@ class ItemController {
   async store({ request, response, auth }) {
     auth = auth.authenticator('jwt')
     const user = auth.user
-    let title = ''
-    let price = 0
-    let url = ''
 
-    request.multipart.field(async (name, value) => {
-      if (name === 'title') {
-        title = value
-      } else if (name === 'price') {
-        price = value
-      }
-    });
-    request.multipart.file('video_file', {
-      types: ["jpg", "jpeg", "png", "gif", "mov", "mp4"],
-      size: "20mb"
-    }, async file => {
-      const filename = `${new Date().getTime()}_${file.clientName}`
-      // set file size from stream byteCount, so adonis can validate file size
-      file.size = file.stream.byteCount
+    const { title, price } = request.all()
+    const file = request.file('video_file')
 
-      // run validation rules
-      await file.runValidations()
 
-      // catches validation errors, if any and then throw exception
-      const error = file.error()
-      if (error.message) {
-        throw new Error(error.message)
-      }
+    await new Promise((resolve) => {
+      const outputPath = "tmp/uploads/clip.mp4";
+      const fileName = `${Date.now()}.${file.subtype}`
 
-      // upload file to s3
-      url = await Drive.put(`user/${user.id}/item/${filename}`, file.stream, {
-        ContentType: file.headers['content-type'],
-        ACL: 'public-read'
-      })
-    })
-    await request.multipart.process()
-    const rules = {
-      title: 'required',
-      price: 'required|number|min:0.01'
-    }
-    const validation = await validate({ title, price }, rules)
-    if (validation.fails()) {
-      return response.send({ result: 'fail' });
-    }
-    console.log(url)
-    const item = await Item.create({ user_id: user.id, title, price, video_path: url, status: 'draft', last_drafted_at: new Date() })
-    await item.save()
+      new ffmpeg(file.tmpPath)
+        .output(outputPath)
+        .outputOptions(['-movflags isml+frag_keyframe'])
+        .toFormat('mp4')
+        .withAudioCodec('copy')
+        .on('error', function (err, stdout, stderr) {
+          console.log('an error happened: ' + err.message);
+          console.log('ffmpeg stdout: ' + stdout);
+          console.log('ffmpeg stderr: ' + stderr);
+        })
+        .on('end', async () => {
+          let stream = await fs.createReadStream(outputPath)
+          const url = await Drive.put(`user/${user.id}/item/${fileName}`, stream, {
+            ContentType: file.headers['content-type'],
+            ACL: 'public-read'
+          })
+          console.log(url)
+          const item = await Item.create({ user_id: user.id, title, price, video_path: url, status: 'draft', last_drafted_at: new Date() })
+          await item.save()
 
+          resolve()
+
+        })
+        .on('progress', function (progress) {
+          console.log('Processing: ' + progress.percent + '% done');
+        }).run()
+
+    }).catch(err => { console.log(err) });
 
     return response.send({ result: 'ok' });
   }
@@ -121,7 +113,7 @@ class ItemController {
     const user = auth.user
     let item = (await Item.query().where({ id: params.id, user_id: user.id }).fetch()).first()
     // await item.delete()
-    item.merge({status: 'deleted', last_deleted_at: new Date() })
+    item.merge({ status: 'deleted', last_deleted_at: new Date() })
     await item.save()
     return response.send({ item });
   }
--- config/bodyParser.js
+++ config/bodyParser.js
@@ -133,7 +133,7 @@ module.exports = {
     | is to true. Otherwise everything is processed manually.
     |
     */
-    processManually: ['/items', '/v1/items']
+    processManually: ['/items']
 
     /*
     |--------------------------------------------------------------------------
